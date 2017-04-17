7a08fb0b45b71b678cd803849127de9f5b684517
Nicolas Dupont <nicolas@akeneo.com>
2017-04-17 12:15:11 +0200

    Remove emitters

--- src/states/Play.ts
+++ src/states/Play.ts
@@ -14,11 +14,9 @@ export default class Play extends Phaser.State {
     private levelProgress: LevelProgress;
     private map;
     private layer;
-    private debug: boolean = false;
+    private debug: boolean = true;
     private seaLevel: number = 450;
     private briefingText : Phaser.BitmapText;
-    private coinLeftEmitter;
-    private coinRightEmitter;
 
     public create()
     {
@@ -82,18 +80,6 @@ export default class Play extends Phaser.State {
         this.minions[0] = new Builder(this.game, 330, 370, 'Builder1', 0);
 
         this.levelProgress = new LevelProgress(this.gnomes, this.hero);
-
-        this.coinLeftEmitter = this.game.add.emitter(0, 80, 1000);
-        this.coinLeftEmitter.bounce.setTo(0.5, 0.5);
-        this.coinLeftEmitter.setXSpeed(100, 500);
-        this.coinLeftEmitter.setYSpeed(-50, 50);
-        this.coinLeftEmitter.makeParticles('coin', [0, 1, 2, 3, 4, 5, 6, 7]);
-
-        this.coinRightEmitter = this.game.add.emitter(800, 80, 1000);
-        this.coinRightEmitter.bounce.setTo(0.5, 0.5);
-        this.coinRightEmitter.setXSpeed(-100, -500);
-        this.coinRightEmitter.setYSpeed(-50, 50);
-        this.coinRightEmitter.makeParticles('coin', [0, 1, 2, 3, 4, 5, 6, 7]);
     }
 
     public update()
@@ -108,8 +94,6 @@ export default class Play extends Phaser.State {
 
         if (this.levelProgress.isFinished()) {
             this.briefingText.text = 'Yeahhhh!! Profit!!!! You finished the game :D';
-            this.coinLeftEmitter.start(false, 5000, 20);
-            this.coinRightEmitter.start(false, 5000, 20);
             this.hero.dance();
             for (let i = 0; i < this.gnomes.length; i++) {
                 this.gnomes[i].dance();
