e92398b1b61589ae551e54758c18283f564a2739
pierallard <pierallard@gmail.com>
2017-05-25 00:06:22 +0200

    [MapGenerator] A little bit of refacto

--- src/map/RandomMapGenerator.ts
+++ src/map/RandomMapGenerator.ts
@@ -3,30 +3,33 @@ import {MapGenerator} from "./MapGenerator";
 
 const GRASS = 35;
 const MNT = 136;
+
 const TILES = [
-    [MNT, MNT, GRASS, MNT, 132],
-    [MNT, MNT, GRASS, GRASS, 133],
-    [MNT, MNT, MNT, GRASS, 134],
-    [MNT, GRASS, GRASS, MNT, 135],
-    [MNT, MNT, MNT, MNT, 136],
-    [GRASS, MNT, MNT, GRASS, 137],
-    [MNT, GRASS, MNT, MNT, 138],
-    [GRASS, GRASS, MNT, MNT, 139],
-    [GRASS, MNT, MNT, MNT, 140],
-    [MNT, GRASS, GRASS, GRASS, 142],
-    [GRASS, MNT, GRASS, GRASS, 143],
-    [GRASS, GRASS, GRASS, MNT, 145],
-    [GRASS, GRASS, MNT, GRASS, 146],
-    [GRASS, GRASS, GRASS, GRASS, 35]
+    [132, MNT, MNT, GRASS, MNT],
+    [133, MNT, MNT, GRASS, GRASS],
+    [134, MNT, MNT, MNT, GRASS],
+    [135, MNT, GRASS, GRASS, MNT],
+    [136, MNT, MNT, MNT, MNT],
+    [137, GRASS, MNT, MNT, GRASS],
+    [138, MNT, GRASS, MNT, MNT],
+    [139, GRASS, GRASS, MNT, MNT],
+    [140, GRASS, MNT, MNT, MNT],
+    [142, MNT, GRASS, GRASS, GRASS],
+    [143, GRASS, MNT, GRASS, GRASS],
+    [145, GRASS, GRASS, GRASS, MNT],
+    [146, GRASS, GRASS, MNT, GRASS],
+    [35, GRASS, GRASS, GRASS, GRASS]
 ];
 
-const DECO = {
-    GRASS: [177, 178, 179, 185, 186, 188, 189, 190],
-    MNT: [200, 201, 202, 203, 204, 205]
-};
+const DECO = [
+    [GRASS, 177, 178, 179, 185, 186, 188, 189, 190],
+    [MNT, 200, 201, 202, 203, 204, 205]
+];
 
 export class RandomMapGenerator extends MapGenerator
 {
+    protected points: Array<Array<number>>;
+
     constructor(game: Phaser.Game, screenWidth: number, screenHeight: number)
     {
         super(game, screenWidth, screenHeight);
@@ -34,8 +37,6 @@ export class RandomMapGenerator extends MapGenerator
 
     generate(): Phaser.Tilemap
     {
-        let x, y: number;
-
         const tileSize = 20;
         const tileSpacing = 20;
 
@@ -45,111 +46,122 @@ export class RandomMapGenerator extends MapGenerator
         this.map.createBlankLayer('layername', this.screenWidth/tileSize, this.screenHeight/tileSize, tileSize, tileSize);
         this.map.addTilesetImage('GrasClif', 'GrasClif', tileSize, tileSize, 0, tileSpacing, 31);
         this.map.addTilesetImage('Grs2Mnt', 'Grs2Mnt', tileSize, tileSize, 0, tileSpacing, 132);
-
         this.map.addTilesetImage('GrssCrtr', 'GrssCrtr', tileSize, tileSize, 0, tileSpacing, 177);
         this.map.addTilesetImage('GrssMisc', 'GrssMisc', tileSize, tileSize, 0, tileSpacing, 180);
         this.map.addTilesetImage('MntMisc', 'MntMisc', tileSize, tileSize, 0, tileSpacing, 200);
 
-        let points = [];
-        for (x = 0; x <= this.screenWidth; x++) {
-            let line = [];
-            for (y = 0; y <= this.screenHeight; y++) {
-                line.push(Math.random() > 0.5 ? GRASS : MNT);
-            }
-            points.push(line);
-        }
-
-        for (x = 0; x <= this.screenWidth; x++) {
-            for (y = 0; y <= this.screenHeight; y++) {
-                let aroundPoints = [
-                    RandomMapGenerator.getPoint(points, x - 1, y - 1),
-                    RandomMapGenerator.getPoint(points, x, y - 1),
-                    RandomMapGenerator.getPoint(points, x + 1, y - 1),
-                    RandomMapGenerator.getPoint(points, x - 1, y),
-                    RandomMapGenerator.getPoint(points, x, y),
-                    RandomMapGenerator.getPoint(points, x + 1, y),
-                    RandomMapGenerator.getPoint(points, x - 1, y + 1),
-                    RandomMapGenerator.getPoint(points, x, y + 1),
-                    RandomMapGenerator.getPoint(points, x + 1, y + 1)
-                ].filter(function (p) {
-                    return null !== p;
-                }).sort();
-                points[x][y] = aroundPoints[Math.floor(aroundPoints.length / 2)];
-            }
-        }
-
-        let maxLoops = 1000;
-        let changes = true;
-        while (maxLoops > 0 && changes) {
-            changes = false;
-            for (x = 0; x < this.screenWidth; x++) {
-                for (y = 0; y < this.screenHeight; y++) {
-                    if (null === RandomMapGenerator.getIndexFromPoints(
-                            RandomMapGenerator.getPoint(points, x, y),
-                            RandomMapGenerator.getPoint(points, x + 1, y),
-                            RandomMapGenerator.getPoint(points, x + 1, y + 1),
-                            RandomMapGenerator.getPoint(points, x, y + 1)
-                        )
-                    ) {
-                        points[x][y] = points[x][y] === MNT ? GRASS : MNT;
-                        changes = true;
-                    }
-                }
-            }
-            maxLoops--;
-        }
-
-        for (x = 0; x < this.screenWidth; x++) {
-            for (y = 0; y <= this.screenHeight; y++) {
-                this.map.putTile(RandomMapGenerator.getDecoratedIndexFromPoints(
-                    RandomMapGenerator.getPoint(points, x, y),
-                    RandomMapGenerator.getPoint(points, x + 1, y),
-                    RandomMapGenerator.getPoint(points, x + 1, y + 1),
-                    RandomMapGenerator.getPoint(points, x, y + 1)
-                ), x, y);
-            }
-        }
+        this.fillRandom();
+        this.smooth();
+        this.fixMissingTextures();
+        this.putTiles();
 
         return this.map;
     }
 
-    private static getDecoratedIndexFromPoints(topLeft: number, topRight: number, bottomRight: number, bottomLeft: number): number {
-        let undecorated = RandomMapGenerator.getIndexFromPoints(topLeft, topRight, bottomRight, bottomLeft);
-        let decorateds = [];
-        if (undecorated === GRASS) {
-            decorateds = DECO.GRASS;
-        } else if (undecorated === MNT) {
-            decorateds = DECO.MNT;
-        }
-        if (decorateds.length && Math.random() < 0.05) {
-            return decorateds[Math.floor(Math.random() * decorateds.length)];
+    private static getDecoratedIndex(topLeft: number, topRight: number, bottomRight: number, bottomLeft: number): number {
+        let undecorated = RandomMapGenerator.getIndex(topLeft, topRight, bottomRight, bottomLeft);
+        for (let i = 0; i < DECO.length; i++) {
+            let decorateds = DECO[i];
+            if (decorateds[0] === undecorated && Math.random() < 0.05) {
+                return decorateds[1 + Math.floor(Math.random() * (decorateds.length - 1))];
+            }
         }
 
         return undecorated;
     }
 
-    private static getIndexFromPoints(topLeft: number, topRight: number, bottomRight: number, bottomLeft: number): number {
+    private static getIndex(topLeft: number, topRight: number, bottomRight: number, bottomLeft: number): number {
         for (let i = 0; i < TILES.length; i++) {
-            if (TILES[i][0] === topLeft
-                && TILES[i][1] === topRight
-                && TILES[i][2] === bottomRight
-                && TILES[i][3] === bottomLeft) {
-                return TILES[i][4];
+            if (TILES[i][1] === topLeft
+                && TILES[i][2] === topRight
+                && TILES[i][3] === bottomRight
+                && TILES[i][4] === bottomLeft) {
+                return TILES[i][0];
             }
         }
 
         return null;
     }
 
-    private static getPoint(points: Array<Array<number>>, x: any, y: number) {
-        if (undefined === points[x]) {
+    private getPoint(x: any, y: number): number {
+        if (undefined === this.points[x]) {
             return null;
         }
 
-        if (undefined === points[x][y]) {
+        if (undefined === this.points[x][y]) {
             return null;
         }
 
-        return points[x][y];
+        return this.points[x][y];
+    }
+
+    private fillRandom() {
+        this.points = [];
+
+        for (let x = 0; x <= this.screenWidth; x++) {
+            let line = [];
+            for (let y = 0; y <= this.screenHeight; y++) {
+                line.push(Math.random() > 0.5 ? GRASS : MNT);
+            }
+            this.points.push(line);
+        }
+    }
+
+    private smooth() {
+        for (let x = 0; x <= this.screenWidth; x++) {
+            for (let y = 0; y <= this.screenHeight; y++) {
+                let aroundPoints = [
+                    this.getPoint(x - 1, y - 1),
+                    this.getPoint(x, y - 1),
+                    this.getPoint(x + 1, y - 1),
+                    this.getPoint(x - 1, y),
+                    this.getPoint(x, y),
+                    this.getPoint(x + 1, y),
+                    this.getPoint(x - 1, y + 1),
+                    this.getPoint(x, y + 1),
+                    this.getPoint(x + 1, y + 1)
+                ].filter(function (p) {
+                    return null !== p;
+                }).sort();
+                this.points[x][y] = aroundPoints[Math.floor(aroundPoints.length / 2)];
+            }
+        }
+    }
+
+    private fixMissingTextures() {
+        let maxLoops = 1000;
+        let changes = true;
+
+        while (maxLoops > 0 && changes) {
+            changes = false;
+            for (let x = 0; x < this.screenWidth; x++) {
+                for (let y = 0; y < this.screenHeight; y++) {
+                    if (null === RandomMapGenerator.getIndex(
+                            this.getPoint(x, y),
+                            this.getPoint(x + 1, y),
+                            this.getPoint(x + 1, y + 1),
+                            this.getPoint(x, y + 1)
+                        )
+                    ) {
+                        this.points[x][y] = this.points[x][y] === MNT ? GRASS : MNT;
+                        changes = true;
+                    }
+                }
+            }
+            maxLoops--;
+        }
+    }
+
+    private putTiles() {
+        for (let x = 0; x < this.screenWidth; x++) {
+            for (let y = 0; y <= this.screenHeight; y++) {
+                this.map.putTile(RandomMapGenerator.getDecoratedIndex(
+                    this.getPoint(x, y),
+                    this.getPoint(x + 1, y),
+                    this.getPoint(x + 1, y + 1),
+                    this.getPoint(x, y + 1)
+                ), x, y);
+            }
+        }
     }
 }
